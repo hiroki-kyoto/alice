286b5e064ba605947c909299bed7220e201b0e6a
Hiroki <xiangchao215@mails.ucas.ac.cn>
2018-06-10 01:09:41 +0800

    3d rendering is ok

--- realworld.py
+++ realworld.py
@@ -8,8 +8,8 @@ from ctypes import *
 def main():
     w, h = 800, 600
     tools = cdll.LoadLibrary('./libtools.so')
-    world_color = np.zeros(h, w, 3, dtype=np.uint8)
-    world_depth = np.zeros(h, w, dtype=np.float32)
+    world_color = np.zeros([h, w, 3], dtype=np.uint8)
+    world_depth = np.zeros([h, w], dtype=np.float32)
     
     if not world_color.flags['CONTIGUOUS']:
         world_color = np.ascontiguous(world_color, world_color.dtype)
@@ -17,13 +17,30 @@ def main():
     if not world_depth.flags['CONTIGUOUS']:
         world_depth = np.ascontiguous(world_depth, world_depth.dtype)
     depth_ptr = cast(world_depth.ctypes.data, POINTER(c_float))
+    
+    # initialize the real world
+    tools.set_world_depth(
+        depth_ptr, 
+        c_int(h), 
+        c_int(w), 
+        c_float(450), 
+        c_float(500),
+        c_float(100))
+    # initialize the pixel map
+    tools.set_pixel_map(
+        color_ptr,
+        depth_ptr,
+        c_int(h),
+        c_int(w),
+        c_int(50),
+        c_int(50))
 
     while True:
         
         cv.imshow('3D World', world_color)
         if cv.waitKey(1) & 0xFF==ord('q'):
             break
-    cap.release()
+    
     cv.destroyAllWindows()
 
 main()
--- tools.c
+++ tools.c
@@ -1,5 +1,86 @@
 #include <math.h>
 
+// A camera and a half of ball which is not solid 
+// world_depth : the depth matrix
+// h : height of world
+// w : width of world
+// r : radius of ball
+// c : center of ball position from the origin
+// f : focus distance
+int set_world_depth(
+    float * world_depth, 
+    int h, 
+    int w, 
+    float r, 
+    float c, 
+    float f
+){
+    int i, j;
+    float x, y;
+    float cx, cy;
+    float t;
+    float delta;
+
+    cx = w/2.0;
+    cy = h/2.0;
+
+    for(i=0; i<h; ++i){
+        for(j=0; j<w; ++j){
+            x = j - cx;
+            y = i - cy;
+            t = (x*x + y*y)/(f*f);
+            delta = t*(r*r-c*c)+r*r;
+            // if the ray does not hit any object
+            if(delta<0){
+                world_depth[i*w+j] = -1.0; // a negative value means infinity
+            } else {
+                world_depth[i*w+j] = 1.0/(t+1)*(c+sqrt(delta));
+            }
+        }
+    }
+}
+
+
+int set_pixel_map(
+    __uint8_t * color_ptr,
+    float * depth_ptr,
+    int h,
+    int w,
+    int uh,
+    int uw
+){
+    int i, j;
+    int row_color_idx, col_color_idx;
+    
+    col_color_idx = row_color_idx = 0;
+    
+    for(i=0; i<h; ++i){
+        if(i%uh==0)
+            row_color_idx = 1 - row_color_idx;
+            col_color_idx = row_color_idx;
+        for(j=0; j<w; ++j){
+            if(j%uw==0)
+                col_color_idx = 1 - col_color_idx;
+            if(depth_ptr[i*w+j]>0){
+                if(col_color_idx){
+                    color_ptr[i*w*3+j*3] = 200; // b
+                    color_ptr[i*w*3+j*3+1] = 0; // g
+                    color_ptr[i*w*3+j*3+2] = 0; // r
+                } else {
+                    color_ptr[i*w*3+j*3] = 200; // b
+                    color_ptr[i*w*3+j*3+1] = 200; // g
+                    color_ptr[i*w*3+j*3+2] = 200; // r
+                }
+            } else {
+                color_ptr[i*w*3+j*3] = 0;
+                color_ptr[i*w*3+j*3+1] = 0;
+                color_ptr[i*w*3+j*3+2] = 0;
+            }
+        }
+    }
+}
+
+
 int change_hair_color(__uint8_t * data, int h, int w, int c){
     int i, j, strides[2]; 
     if(c!=3){ return 1; }
