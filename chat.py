5785d76db964126a9762aec7217351e37c2d1e2a
Hiroki <yamasaki_hiroki_kyoto@yahoo.co.jp>
2018-11-23 16:35:11 +0800

    remember my name

--- chat.py
+++ chat.py
@@ -12,14 +12,20 @@ if __name__ == '__main__':
     me = 'ウシオ'
     you = 'あなた'
     print("おはようございます！")
-    time.sleep(1)
+    time.sleep(3)
     print('今日はいい天気だか？')
-    time.sleep(1)
+    time.sleep(3)
     print('%sはこれからよろしくね！' % me)
-    time.sleep(1)
+    time.sleep(3)
     print('失礼ですけと、%sの名前は？' % you)
-    s = receive()
-    print('じゃ、これから%sと呼んで喜んでしょうか？' % s)
-    s = receive()
-    if s.startswith('はい') or s.startswith('いい'):
-        print('ご')
+    while True:
+        s = receive()
+        print('じゃ、これから%sと呼んで喜んでしょうか？' % s)
+        s = receive()
+        if s == 'はい' or s =='いい':
+            print('さて、これで自己紹介は終わりです！一緒に楽しみにお話してくださいね！')
+            break
+        else:
+            print('そうか、ごめん！本当のお名前は教えてくださいね！')
+    print('終わり。。。')
+    
