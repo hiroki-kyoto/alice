7a968ad67d0ab879d9962e402f0195e799ea6d8b
Chao Xiang <xiangchao215@mails.ucas.ac.cn>
2019-03-09 21:04:19 +0800

    macro2micro-image2class

---
+++ macro2micro_image2class.py
@@ -0,0 +1,12 @@
+# filename: macro2micro_image2class.py
+
+import numpy as np
+import tensorflow as tf
+
+
+def model_macro2micro_image2class(input_):
+    pass
+
+
+def main():
+    input_ = tf.placeholder(dtype=tf.float32, shape=[])
