7504d4cddef80ebd1e04238707db7b95da637f7c
Chao Xiang <xiangchao215@mails.ucas.ac.cn>
2019-03-10 13:49:14 +0800

    a bit update

--- macro2micro_image2class.py
+++ macro2micro_image2class.py
@@ -4,9 +4,15 @@ import numpy as np
 import tensorflow as tf
 
 
-def model_macro2micro_image2class(input_):
-    pass
+def model_macro2micro_image2class(input_, depth):
+    shape_of_input = input_.shape.as_list()
+    assert len(shape_of_input) == 4
+    h, w = shape_of_input[1], shape_of_input[2]
+    # resize if needed
+    output_ = None
+    return output_
 
 
 def main():
-    input_ = tf.placeholder(dtype=tf.float32, shape=[])
+    in_ = tf.placeholder(dtype=tf.float32, shape=[1, 32, 32, 1])
+    out_ = model_macro2micro_image2class(in_, 3)
