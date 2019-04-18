e4f3e9c90189afa57fcc676ca9be554c9ca83e81
Chao Xiang <xiangchao215@mails.ucas.ac.cn>
2019-04-18 22:05:49 +0800

    build simulator

--- count.py
+++ count.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 import struct
 import os
 from PIL import Image
+import glob
 
 
 def conv2d(input_, ksize, stride, out_channels, scope):
@@ -93,11 +94,11 @@ def encoder(input_, scope_):
     return out_
 
 
-def decoder(input_, scope_):
+def decoder(input_, scope_, h, w):
     out_ = fully_connect(input_, 8, scope_ + '/decoder/fc1')
     batch_ = out_.shape.as_list()[0]
-    h_ = 7
-    w_ = 7
+    h_ = h // 4
+    w_ = w // 4
     c_ = 4
     channels = int(h_ * w_ * c_)
     out_ = fully_connect(out_, channels, scope_ + '/decoder/fc2')
@@ -131,7 +132,7 @@ def gray2rgb(gray):
     return np.stack([gray, gray, gray], axis=-1)
 
 
-if __name__ == '__main__':
+def train_mnist():
     # load the dataset
     data_dir = 'E:/CodeHub/Datasets/MNIST'
     train_image_path = os.path.join(data_dir, 'train-images.idx3-ubyte')
@@ -150,9 +151,9 @@ if __name__ == '__main__':
     # build the network
     t_in_ = tf.placeholder(dtype=tf.float32, shape=[1, ims.shape[1], ims.shape[2], 1])
     # build encoder
-    t_features = encoder(t_in_, scope_='mnist')
+    t_features = encoder(t_in_, 'mnist')
     print(t_features.shape.as_list())
-    t_in_rec = decoder(t_features, scope_='mnist')
+    t_in_rec = decoder(t_features, 'mnist', ims.shape[1], ims.shape[2])
     t_loss = tf.reduce_mean(tf.abs(t_in_ - t_in_rec))
     t_opt = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(t_loss)
 
@@ -166,15 +167,18 @@ if __name__ == '__main__':
     else:
         sess.run(tf.global_variables_initializer())
 
-    # test the result
-    id_ = 0
-    out_ = sess.run(t_in_rec, feed_dict={
-        t_in_: np.expand_dims(np.expand_dims(ims[ids[id_]], axis=0), axis=-1)})
-    out_ = np.maximum(np.minimum(out_, 1.0), 0.0)
-    out_ = out_[0, :, :, 0]
-    merged_ = np.concatenate((ims[ids[id_]], out_), axis=1)
-    Image.fromarray(gray2rgb(np.uint8(merged_ * 255))).show()
-    exit(0)
+    # test the result with latent interpolation
+    # fea_0 = sess.run(t_features, feed_dict={
+    #     t_in_: np.expand_dims(np.expand_dims(ims[ids[7]], axis=0), axis=-1)})
+    # fea_1 = sess.run(t_features, feed_dict={
+    #     t_in_: np.expand_dims(np.expand_dims(ims[ids[8]], axis=0), axis=-1)})
+    # out_ = sess.run(t_in_rec, feed_dict={
+    #     t_features: 0.5 * fea_0 + 0.5 * fea_1
+    # })
+    # out_ = np.maximum(np.minimum(out_, 1.0), 0.0)
+    # out_ = out_[0, :, :, 0]
+    # Image.fromarray(gray2rgb(np.uint8(out_ * 255))).show()
+    # exit(0)
 
     loss_av_ = np.zeros([500], dtype=np.float32)
     for i in range(repeats):
@@ -195,3 +199,217 @@ if __name__ == '__main__':
 
     # stage II: enhance autoencoder with self produced input
     # stage III: recursively repeat such training until AE converged.
+
+
+def train_drawing():
+    # load the dataset
+    data_dir = 'E:/CodeHub/Datasets/Drawing'
+    train_image_path = glob.glob(data_dir + '/*.jpg')
+    ims = []
+    for path_ in train_image_path:
+        ims.append(np.array(Image.open(path_), dtype=np.float32)[:, :, 0] / 255.0)
+    ims = np.stack(ims, axis=0)
+
+    # build the network
+    t_in_ = tf.placeholder(dtype=tf.float32, shape=[1, ims.shape[1], ims.shape[2], 1])
+    # build encoder
+    t_features = encoder(t_in_, 'drawing')
+    print(t_features.shape.as_list())
+    t_in_rec = decoder(t_features, 'drawing', ims.shape[1], ims.shape[2])
+    t_loss = tf.reduce_mean(tf.abs(t_in_ - t_in_rec))
+    t_opt = tf.train.AdamOptimizer(learning_rate=1e-3).minimize(t_loss)
+
+    # train the model with data
+    repeats = 500000
+    sess = tf.Session()
+    saver = tf.train.Saver()
+    ckpt_prefix = 'models/drawing.ckpt'
+    if tf.train.checkpoint_exists(ckpt_prefix):
+        saver.restore(sess, ckpt_prefix)
+    else:
+        sess.run(tf.global_variables_initializer())
+
+    # test the result with latent interpolation
+    # out_ = sess.run(t_in_rec, feed_dict={
+    #     t_in_: np.expand_dims(np.expand_dims(ims[ids[7]], axis=0), axis=-1)
+    # })
+    # out_ = np.maximum(np.minimum(out_, 1.0), 0.0)
+    # out_ = out_[0, :, :, 0]
+    # Image.fromarray(gray2rgb(np.uint8(out_ * 255))).show()
+    # exit(0)
+
+    loss_av_ = np.zeros([500], dtype=np.float32)
+    for i in range(repeats):
+        id_ = np.random.randint(len(ims))
+        loss_, _, out_ = sess.run([t_loss, t_opt, t_in_rec], feed_dict={
+            t_in_: np.expand_dims(np.expand_dims(ims[id_], axis=0), axis=-1)
+        })
+        loss_av_[i % len(loss_av_)] = loss_
+
+        if i % 10000 == 0:
+            print('#%d\t loss: %f' % (i, loss_av_[np.where(loss_av_)].mean()))
+            # save the reconstructed image
+            out_ = np.maximum(np.minimum(out_, 1.0), 0.0)
+            Image.fromarray(
+                gray2rgb(
+                    np.uint8(out_[0, :, :, 0] * 255)
+                )
+            ).save('shots/rec_%d.jpg' % i)
+
+    # save the model
+    saver.save(sess, ckpt_prefix)
+
+
+def connect_encoders(fea_a, fea_b):
+    out_a2b = fully_connect(fea_a, fea_a.shape[1] * 2, 'connector/a2b/fc1')
+    out_a2b = fully_connect(out_a2b, fea_a.shape[1] * 4, 'connector/a2b/fc2')
+    out_a2b = fully_connect(out_a2b, fea_a.shape[1] * 2, 'connector/a2b/fc3')
+    out_a2b = fully_connect(out_a2b, fea_b.shape[1], 'connector/a2b/fc4')
+
+    out_b2a = fully_connect(fea_b, fea_b.shape[1] * 2, 'connector/b2a/fc1')
+    out_b2a = fully_connect(out_b2a, fea_b.shape[1] * 4, 'connector/b2a/fc2')
+    out_b2a = fully_connect(out_b2a, fea_b.shape[1] * 2, 'connector/b2a/fc3')
+    out_b2a = fully_connect(out_b2a, fea_a.shape[1], 'connector/b2a/fc4')
+    return out_a2b, out_b2a
+
+
+def initialize_unset_vars(sess):
+    global_vars = tf.global_variables()
+    bool_inits = sess.run([tf.is_variable_initialized(var) for var in global_vars])
+    uninit_vars = [v for (v, b) in zip(global_vars, bool_inits) if not b]
+    for v in uninit_vars:
+        print(str(v.name))
+    if len(uninit_vars):
+        sess.run(tf.variables_initializer(uninit_vars))
+
+
+def train_connector():
+    # mnist dataset
+    data_dir = 'E:/CodeHub/Datasets/MNIST'
+    train_image_path = os.path.join(data_dir, 'train-images.idx3-ubyte')
+    train_label_path = os.path.join(data_dir, 'train-labels.idx1-ubyte')
+    ims_mnist = read_images(train_image_path)
+    lbs_mnist = read_labels(train_label_path)
+    ids_mnist = [-1] * 10
+    i = 0
+    while np.min(ids_mnist) < 0:
+        id_ = np.argmax(lbs_mnist[i])
+        ids_mnist[id_] = i
+        i += 1
+
+    # drawing dataset
+    data_dir = 'E:/CodeHub/Datasets/Drawing'
+    train_image_path = glob.glob(data_dir + '/*.jpg')
+    ims_drawing = []
+    for path_ in train_image_path:
+        ims_drawing.append(np.array(Image.open(path_), dtype=np.float32)[:, :, 0] / 255.0)
+    ims_drawing = np.stack(ims_drawing, axis=0)
+
+    t_in_mnist = tf.placeholder(
+        dtype=tf.float32,
+        shape=[1, ims_mnist.shape[1], ims_mnist.shape[2], 1])
+    t_fea_mnist = encoder(t_in_mnist, 'mnist')
+
+    t_in_drawing = tf.placeholder(
+        dtype=tf.float32,
+        shape=[1, ims_drawing.shape[1], ims_drawing.shape[2], 1])
+    t_fea_drawing = encoder(t_in_drawing, 'drawing')
+
+    out_a2b, out_b2a = connect_encoders(t_fea_mnist, t_fea_drawing)
+
+    t_out_mnist = decoder(
+        out_b2a,
+        'mnist',
+        ims_mnist.shape[1],
+        ims_mnist.shape[2])
+
+    t_out_drawing = decoder(
+        out_a2b,
+        'drawing',
+        ims_drawing.shape[1],
+        ims_drawing.shape[2])
+
+    loss_a2b = tf.reduce_mean(tf.abs(out_a2b - t_fea_drawing))
+    loss_b2a = tf.reduce_mean(tf.abs(out_b2a - t_fea_mnist))
+    loss_overall = loss_a2b + loss_b2a
+
+    all_vars = tf.trainable_variables()
+    train_vars = []
+    for v in all_vars:
+        if v.name.startswith('connector'):
+            train_vars.append(v)
+            print(v.name)
+
+    minimizer_ = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(
+        loss_overall,
+        var_list=train_vars
+    )
+
+    repeats = 500000
+    sess = tf.Session()
+    ckpt_prefix = 'models/connected.ckpt'
+    if tf.train.checkpoint_exists(ckpt_prefix):
+        saver = tf.train.Saver(tf.global_variables())
+        saver.restore(sess, ckpt_prefix)
+    else:
+        # initialize the mnist model
+        vars_mnist = dict()
+        for v in all_vars:
+            if v.name.startswith('mnist'):
+                vars_mnist[v.name[:-2]] = v
+        saver = tf.train.Saver(var_list=vars_mnist)
+        saver.restore(sess, 'models/mnist.ckpt')
+        # initialize the drawing model
+        vars_drawing = dict()
+        for v in all_vars:
+            if v.name.startswith('drawing'):
+                vars_drawing[v.name[:-2]] = v
+        saver = tf.train.Saver(var_list=vars_drawing)
+        saver.restore(sess, 'models/drawing.ckpt')
+        initialize_unset_vars(sess)
+
+    loss_av_ = np.zeros([500], dtype=np.float32)
+    for i in range(repeats):
+        id_ = np.random.randint(len(ids_mnist))
+        loss_, _, out_mnist, out_drawing = sess.run([
+            loss_overall,
+            minimizer_,
+            t_out_mnist,
+            t_out_drawing
+        ], feed_dict={
+            t_in_mnist: np.expand_dims(
+                np.expand_dims(
+                    ims_mnist[ids_mnist[id_]],
+                    axis=0),
+                axis=-1),
+            t_in_drawing: np.expand_dims(
+                np.expand_dims(
+                    ims_drawing[id_],
+                    axis=0),
+                axis=-1)
+        })
+        loss_av_[i % len(loss_av_)] = loss_
+
+        if i % 10000 == 0:
+            print('#%d\t loss: %f' % (i, loss_av_[np.where(loss_av_)].mean()))
+            # save the reconstructed image
+            out_mnist_sup = np.zeros([out_drawing.shape[1], out_drawing.shape[2]])
+            out_mnist_sup[:out_mnist.shape[1], :out_mnist.shape[2]] = out_mnist[0, :, :, 0]
+            out_drawing = out_drawing[0, :, :, 0]
+            out_ = np.concatenate((out_mnist_sup, out_drawing), axis=1)
+            out_ = np.maximum(np.minimum(out_, 1.0), 0.0)
+            Image.fromarray(
+                gray2rgb(
+                    np.uint8(out_ * 255)
+                )
+            ).save('shots/rec_%d.jpg' % i)
+
+    # save the model
+    saver = tf.train.Saver(tf.global_variables())
+    saver.save(sess, ckpt_prefix)
+
+
+if __name__ == '__main__':
+    train_mnist()
+    # train_drawing()
+    # train_connector()
---
+++ handwrite.py
@@ -0,0 +1,91 @@
+# handwrite.py
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+
+# canvas setting: canvas height and width, and pen radius
+h, w = 256, 256
+r = w // 32
+color_bound = 0.5
+sim_c = 0.5 # the speed of light in simulation: the maximum of speed enabled
+sim_d = 1.0/w # the minimum of simulation in space
+sim_t = sim_d / sim_c
+num_moves = 128
+
+
+def depth2color(depth):
+    if depth < color_bound:
+        return depth / color_bound
+    else:
+        return 1.0
+
+
+def dot(bmp, x, y, p):
+    x_int = int(x * w)
+    y_int = int(y * h)
+    p_int = int(p * r)
+    if p > 0:
+        for i in range(y_int - p_int, y_int + p_int + 1):
+            for j in range(x_int - p_int, x_int + p_int + 1):
+                if 0 <= i < h and 0 <= j < w:
+                    if (i - y_int) * (i - y_int) + (j - x_int) * (j - x_int) <= p_int * p_int:
+                        bmp[i, j] = np.minimum(1.0, bmp[i, j] + depth2color(p))
+
+
+# draw black lines on white sheet
+def draw_on_sheet(start_, moves):
+    # start_ includes initial position, pressure.
+    # moves includes: acceleration over position and pressure.
+    # the velocity and position over sheet surface and along the direction
+    # erected to the sheet.
+    x, y, p = start_[0], start_[1], start_[2]
+    bmp = np.zeros([h, w], dtype=np.float32)
+    v_x, v_y, v_p = 0, 0, 0
+    dot(bmp, x, y, p)
+    last_x = x
+    last_y = y
+    for m_ in moves:
+        a_x, a_y, a_p = m_[0], m_[1], m_[2]
+        for t in np.arange(0, 1, sim_t):
+            x_t = x + v_x * t + 0.5 * a_x * t * t
+            y_t = y + v_y * t + 0.5 * a_y * t * t
+            p_t = p + v_p * t + 0.5 * a_p * t * t
+            if x_t != last_x or y_t == last_y:
+                dot(bmp, x_t, y_t, p_t)
+                last_x = x_t
+                last_y = y_t
+        x = x + v_x + 0.5 * a_x
+        y = y + v_y + 0.5 * a_y
+        p = p + v_p + 0.5 * a_p
+        v_x = v_x + a_x
+        v_y = v_y + a_y
+        v_p = v_p + a_p
+        assert np.abs(v_x) < sim_c
+        assert np.abs(v_y) < sim_c
+    return bmp
+
+
+def simulator():
+    t_in = tf.placeholder(dtype=tf.float32, shape=[1, 1, num_moves, 3])
+    
+
+
+
+if __name__ == '__main__':
+    bmp = draw_on_sheet(
+        [0.33, 0.33, 0.0],
+        [
+            [0.01, 0.02, 0.2],
+            [0.0, -0.02, 0.0],
+            [0.3, -0.02, -0.07],
+            [-0.3, 0.04, 0.03],
+            [-0.1, 0.25, -0.5],
+            [-0.05, 0.0, 0.5],
+            [0.1, -0.7, -1.0],
+            [0.1, 0.0, 1.0],
+            [0.05, 0.6, 0.8],
+            [-0.35, 0.3, -1.7],
+            [0.0, -0.3, 1.0]
+        ])
+    print(bmp.shape)
+    Image.fromarray(np.uint8(bmp*255)).show()
