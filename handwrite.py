60cf9c6a1b427786dec75447463a02296899fde6
hiroki-kyoto <xiangchao215@mails.ucas.ac.cn>
2019-04-26 18:17:44 +0800

    stae predictor

--- handwrite.py
+++ handwrite.py
@@ -57,8 +57,6 @@ def update_sheet(bmp_, pos_, vel_, mov_):
     v_x = v_x + a_x
     v_y = v_y + a_y
     v_p = v_p + a_p
-    # assert np.abs(v_x) < sim_c
-    # assert np.abs(v_y) < sim_c
     if p > 1 or p < 0:
         v_p = 0
         p = np.minimum(np.maximum(p, 0), 1)
@@ -203,7 +201,7 @@ def states_decoder(t_feat_merged):
             name_or_scope='states/decoder',
             reuse=tf.AUTO_REUSE):
         t_out = dense_block(t_feat_merged, [8, 16], True)
-        t_out = dense_block(t_out, [8, 6], True)
+        t_out = dense_block(t_out, [8, 6], False)
         # reshape into 2-dimension array in such form:
         # [[v_x, v_y, v_p], [x, y, p]], a 2x3 array.
         t_out = tf.reshape(t_out, shape=[1, 1, 2, 3])
@@ -274,11 +272,9 @@ class Simulator:
         self.t_loss_observ = tf.reduce_mean(
             tf.abs(self.t_pred_observ - self.t_next_observ))
         alpha = 1.0
-        self.t_loss_global = self.t_loss_states * alpha \
-                             + self.t_loss_observ * (1 - alpha)
+        self.t_loss_global = self.t_loss_states * alpha + self.t_loss_observ * (1 - alpha)
 
-        self.t_opt = tf.train.AdamOptimizer(learning_rate=1e-3)\
-            .minimize(self.t_loss_global)
+        self.t_opt = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(self.t_loss_global)
         self.sess = tf.Session()
 
     def train(self, model_path, dump_path):
@@ -299,6 +295,9 @@ class Simulator:
 
         states = np.stack([vel, pos], axis=0)
         states_last = np.copy(states)
+
+        loss_s_av = 0
+        loss_o_av = 0
 
         for i in range(train_step):
             if np.random.rand() < reset_prob:
@@ -331,10 +330,24 @@ class Simulator:
                     self.t_next_observ: expand_dims(bmp, axises=[0, -1]),
                 }
             )
+
+            m = 100.0
+            if i < m:
+                loss_s_av = loss_s_av * (i / m) + loss_s * (1 - i / m)
+                loss_o_av = loss_o_av * (i / m) + loss_o * (1 - i / m)
+            else:
+                loss_s_av = loss_s_av * ((m - 1) / m) + loss_s * (1 / m)
+                loss_o_av = loss_o_av * ((m - 1) / m) + loss_o * (1 / m)
+
             if i % 1000 == 0:
-                print("Itr=%d States=%.5f Observ=%.5f" % (i, loss_s, loss_o))
+                print("Itr=%d States=%.5f Observ=%.5f" % (i, loss_s_av, loss_o_av))
                 bmp_merged = merge_bmp(bmp, cut(pred[0, :, :, 0]))
                 save_bmp(bmp_merged, i, dump_path)
+                # print('acceleration=%s' % str(action_))
+                # print('previous velocity=%s' % str(states_last[0, :]))
+                # print('previous position=%s' % str(states_last[1, :]))
+                # print('velocity=%s' % str(states[0, :]))
+                # print('position=%s' % str(states[1, :]))
 
         saver.save(self.sess, model_path)
 
@@ -345,28 +358,138 @@ class Simulator:
         pass
 
 
+class StatePredictor:
+    def __init__(self):
+        self.t_action = tf.placeholder(dtype=tf.float32, shape=[1, 3])
+        self.t_states = tf.placeholder(dtype=tf.float32, shape=[2, 3])
+        self.t_next_states = tf.placeholder(dtype=tf.float32, shape=[2, 3])
+
+        t_feat = tf.concat((self.t_action, self.t_states), axis=0)
+        t_feat = tf.reshape(t_feat, shape=[1, 9])
+        print(t_feat.shape.as_list())
+
+        t_feat = tf.layers.dense(
+            t_feat,
+            8,
+            act_fn(),
+            True,
+            kernel_initializer=ini_fn())
+        t_feat = tf.layers.dense(
+            t_feat,
+            8,
+            act_fn(),
+            True,
+            kernel_initializer=ini_fn())
+        t_feat = tf.layers.dense(
+            t_feat,
+            6,
+            act_fn(),
+            True,
+            kernel_initializer=ini_fn())
+
+        self.t_pred_states = tf.reshape(t_feat, shape=[2, 3])
+
+        self.t_loss = tf.reduce_mean(tf.abs(self.t_pred_states - self.t_next_states))
+
+        self.t_opt = tf.train.AdamOptimizer(learning_rate=1e-3).minimize(self.t_loss)
+        self.sess = tf.Session()
+
+    def train(self, model_path, dump_path):
+        saver = tf.train.Saver()
+        if tf.train.checkpoint_exists(model_path):
+            saver.restore(self.sess, model_path)
+        else:
+            self.sess.run(tf.global_variables_initializer())
+
+        train_step = 100000
+        reset_prob = 0.01
+
+        pos = np.random.rand(3)
+        vel = np.random.rand(3)
+
+        states = np.stack([vel, pos], axis=0)
+        states_last = np.copy(states)
+
+        loss_av = 0
+
+        for i in range(train_step):
+            if np.random.rand() < reset_prob:
+                pos = np.random.rand(3)
+                vel = np.random.rand(3)
+                states[0, :] = vel[:]
+                states[1, :] = pos[:]
+
+            states_last[:, :] = states[:, :]
+
+            action_ = np.random.rand(3) - 0.5
+            action_[:2] = 0.05 * action_[:2]
+            action_[2] = 0.5 * action_[2]
+
+            # update the states with physical rules
+            pos = pos + vel + 0.5 * action_
+            vel = vel + action_
+            valid_mask = np.float32(pos >= 0)
+            valid_mask = valid_mask * np.float32(pos <= 1)
+            vel = vel * valid_mask
+            pos = np.maximum(np.minimum(pos, 1), 0)
+
+            states[0, :] = vel[:]
+            states[1, :] = pos[:]
+
+            pred, _, loss = self.sess.run(
+                [
+                    self.t_pred_states,
+                    self.t_opt,
+                    self.t_loss
+                 ],
+                feed_dict={
+                    self.t_action: expand_dims(action_, axises=[0]),
+                    self.t_states: states_last,
+                    self.t_next_states: states
+                }
+            )
+
+            m = 100.0
+            if i < m:
+                loss_av = loss_av * (i / m) + loss * (1 - i / m)
+            else:
+                loss_av = loss_av * ((m - 1) / m) + loss * (1 / m)
+
+            if i % 1000 == 0:
+                print("Itr=%d Loss=%.5f" % (i, loss_av))
+                # print('acceleration=%s' % str(action_))
+                # print('previous velocity=%s' % str(states_last[0, :]))
+                # print('previous position=%s' % str(states_last[1, :]))
+                # print('velocity=%s' % str(states[0, :]))
+                # print('position=%s' % str(states[1, :]))
+
+        saver.save(self.sess, model_path)
+
+
 if __name__ == '__main__':
     # bmp = np.zeros([h, w], dtype=np.float32)
-    # bmp = draw_on_sheet(
-    #     [0.3, 0.3, 0.0],
-    #     [
-    #         [0.01, 0.02, 0.2],
-    #         [0.0, -0.02, 0.0],
-    #         [0.3, -0.02, -0.07],
-    #         [-0.3, 0.04, 0.03],
-    #         [-0.1, 0.25, -0.5],
-    #         [-0.05, 0.0, 0.5],
-    #         [0.1, -0.7, -1.0],
-    #         [0.1, 0.0, 1.0],
-    #         [0.05, 0.6, 0.8],
-    #         [-0.35, 0.3, -1.5],
-    #         [0.0, -0.3, 1.0]
-    #     ])
-    # pos = np.random.rand(3)
-    # moves = 0.1 * (np.random.rand(100, 3) - 0.3)
-    # bmp = draw_on_sheet(pos, moves)
-    # Image.fromarray(np.uint8(bmp*255)).show()
-    sim = Simulator()
-    samples = []
-    # cache samples in separate threads
-    sim.train('models/simulator.ckpt', 'shots')
+    # pos = np.array([0.3, 0.3, 0.0])
+    # moves = np.array([
+    #     [0.01, 0.02, 0.2],
+    #     [0.0, -0.02, 0.0],
+    #     [0.3, -0.02, -0.07],
+    #     [-0.3, 0.04, 0.03],
+    #     [-0.1, 0.25, -0.5],
+    #     [-0.05, 0.0, 0.5],
+    #     [0.1, -0.7, -1.0],
+    #     [0.1, 0.0, 1.0],
+    #     [0.05, 0.6, 0.8],
+    #     [-0.35, 0.3, -1.5],
+    #     [0.0, -0.3, 1.0]
+    # ])
+    # vel = np.zeros([3])
+    # for mov_ in moves:
+    #     update_sheet(bmp, pos, vel, mov_)
+    # visualize_bmp(bmp)
+
+    # sim = Simulator()
+    # sim.train('models/simulator.ckpt', 'shots')
+
+    state_predictor = StatePredictor()
+    state_predictor.train('models/state_predictor.ckpt',  'shots')
+
