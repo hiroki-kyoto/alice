e6fbc109bb26a65710e46625b50c9993dcc0c419
Chao Xiang <xiangchao215@mails.ucas.ac.cn>
2019-05-22 21:52:06 +0800

    train model

--- happy_loop.py
+++ happy_loop.py
@@ -484,9 +484,102 @@ class Solver:
             t_guess_y = tf.nn.softmax(t_guess_y, axis=-1)
             t_guess_z = tf.nn.softmax(t_guess_z, axis=-1)
             print(t_guess_x.shape)
-            self.t_guess_action = ...
+            self.t_guess_action = tf.concat(
+                [t_guess_x, t_guess_y, t_guess_z],
+                axis=-1
+            )
+        # guess action is initial solution for action solver.
+        # however, solved action behaves as a supervisor for guess model.
+        self.t_loss_guess = tf.reduce_mean(
+            tf.abs(self.t_guess_action - self.t_action))
+        self.t_opt_guess = tf.train.AdamOptimizer(
+            learning_rate=1e-3).minimize(self.t_loss_guess)
 
         self.sess = tf.Session()
+
+    def train(self, model_path, dump_path):
+        saver = tf.train.Saver()
+        ============ initialize the uninitialized variables ======
+        if tf.train.checkpoint_exists(model_path):
+            saver.restore(self.sess, model_path)
+        else:
+            self.sess.run(tf.global_variables_initializer())
+
+        train_sessions = 1000
+        train_episodes = 100
+        train_steps = 30
+        loss_cache = np.zeros([100])
+        loss_means = []
+        loss_varis = []
+        counter = 0
+
+        plt.ion()
+        plt.figure(1)
+
+        for _ in range(train_sessions):
+            hit_wall = False
+            im = np.zeros([h, w], dtype=np.float32)
+            pos = np.array([0.5, 0.5, 0.0])
+            moves = action_generator(train_episodes)
+            for i in range(train_episodes):
+                if hit_wall:
+                    break
+                steps_ = int(train_steps * np.random.rand())
+                for _ in range(steps_):
+                    ppos = [int(pos[0]*w), int(pos[1]*h)]
+                    pbeg = [ppos[0] - self.patch_r,
+                            ppos[1] - self.patch_r]
+                    pend = [ppos[0] + self.patch_r + 1,
+                            ppos[1] + self.patch_r + 1]
+                    if pbeg[0] < 0 or pbeg[1] < 0\
+                        or pend[0] > w or pend[1] > h:
+                        hit_wall = True
+                        print("===== hit the wall, start new session =====")
+                        break
+                    curr = np.copy(im[pbeg[1]:pend[1], pbeg[0]:pend[0]])
+                    im, pos = render_step(im, pos, moves[i])
+                    next = np.copy(im[pbeg[1]:pend[1], pbeg[0]:pend[0]])
+
+                    _, loss = self.sess.run([
+                        self.t_opt,
+                        self.t_loss],
+                        feed_dict={
+                            self.t_action: expand_dims(
+                                index_to_onehot(
+                                    moves[i],
+                                    [len(_M_X), len(_M_Y), len(_M_Z)]),
+                                axises=[0]),
+                            self.t_observ: np.reshape(
+                                curr,
+                                [1, self.patch_h * self.patch_w]),
+                            self.t_next_observ: np.reshape(
+                                next,
+                                [1, self.patch_h * self.patch_w])
+                        })
+
+                    loss_cache[counter % len(loss_cache)] = loss
+
+                    if (counter + 1) % 100 == 0:
+                        loss_means.append(np.mean(loss_cache))
+                        loss_varis.append(np.sqrt(np.sum(
+                            np.square(loss_cache - loss_means[-1])) /
+                                                  (len(loss_cache) - 1)))
+                        plt.clf()
+                        plt.plot(
+                            range(len(loss_means)),
+                            loss_means,
+                            '-*',
+                            label='mean')
+                        plt.plot(
+                            range(len(loss_varis)),
+                            loss_varis,
+                            '+-',
+                            label='vari')
+                        plt.legend()
+                        plt.pause(0.01)
+                    if (counter + 1) % 1000 == 0:
+                        saver.save(self.sess, model_path)
+                    counter += 1
 
 
 def test_dynamic_disp():
